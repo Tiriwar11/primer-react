e99145aa0d10bcf861f13cf55886dd35009753f8
Yeferson Stiwar Salazar Guerra <stiwarsg11@gmail.com>
2023-12-11 17:21:48 -0500

    Hola

---
+++ src/components/Card2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () => { // funcion flecha
+    return ( // parte HTML
+        <React.Fragment> 
+            <h3>Hola Mundo</h3> 
+            <button>Yo</button>
+            <p>Hola soy sebas</p>
+
+        </React.Fragment>
+    )
+}
+;

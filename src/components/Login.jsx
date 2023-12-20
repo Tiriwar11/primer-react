3aeeff0aab46e59819f3a89982a8783cbd0d0c82
BastianSss <sebastian.suaza16@gmail.com>
2023-12-19 21:22:01 -0500

    Se agrego check y se importaron iconos

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,6 @@
+import { FaUser } from "react-icons/fa"; //Importaciones para utilizar iconos
+import { FaLock } from "react-icons/fa"; // npm install react-icons --save
+
 import React from 'react';
 import './Login.css'
 const Login = () => {
@@ -7,10 +10,19 @@ const Login = () => {
                 <h1>Login</h1>
                 <div className="input-box">
                     <input type="text" placeholder='Usuario' required />
+                    <FaUser className="icon" />
                 </div>
                 <div className="input-box">
                     <input type="password" placeholder='Contraseña' required />
+                    <FaLock className="icon" />
                 </div>
+                <div className='renember-forgot'>
+                    <label><input type='checkbox'/>Recordar</label>
+                    <a href='#'> ¿Olvido Su contraseña? </a>
+                </div>
+
+                <button type="submit"> Login </button>
+                <button type="submit"> Sign Up </button>
             </form>
         </div>
     );
